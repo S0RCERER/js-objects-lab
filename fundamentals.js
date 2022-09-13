c6d4f75de7a6d3844b10a5939bb35f9ea6213ca6
Ling Feng <you@example.com>
2022-09-13 12:39:50 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -11,7 +11,6 @@ const aCar = {
         number: 1058 }
 };
 
-
 console.log(aCar.owner + ' drives a ' + aCar.type.make);
 console.log(
     "Reg. = " +
@@ -21,3 +20,19 @@ console.log(
       "-" +
       aCar.registration.number
   );
+
+aCar.milage = 10000
+aCar.color = {
+  exterior: "red",
+  interior: { texture: "leather", shade: "cream" }
+}
+
+console.log(
+  "It is a " +
+    aCar.color.exterior +
+    " car, " +
+    aCar.milage +
+    " milage, with " +
+    aCar.color.interior.texture +
+    " interior."
+);
